d3824a9358bdfe46f4fc51ccb025ecbab6edf9c2
Damien George <damien.p.george@gmail.com>
2013-02-15 10:36:41 +0000

    Improved convert-userdata script; converted Thomas' data because he asked.

--- convert-userdata.py
+++ convert-userdata.py
@@ -2,10 +2,13 @@
 Convert old v:4 papers column to new JSON (tags,notes,graphs) columns.
 """
 
+# we have converted: Thomas' data
+
 import os
 import os.path
 import argparse
 import hashlib
+import json
 from xiwi.common import mysql
 
 def executeSqlQuery(dbCursor, dryRun, cmd, arg):
@@ -62,18 +65,29 @@ class MiniLexer(object):
         return self.__str[start:end]
 
 def doWork(dbCursor, usermail, dryRun):
-    hits = dbCursor.execute("SELECT papers FROM userdata WHERE usermail=%s", (usermail,))
+    hits = dbCursor.execute("SELECT papers,tags,notes,graphs FROM userdata WHERE usermail=%s", (usermail,))
     if hits != 1:
         print "ERROR: unknown usermail {}".format(usermail)
         return
 
     # parse papers column
 
-    papers = dbCursor.fetchone()[0]
+    row = dbCursor.fetchone()
+    papers = row[0]
     if not papers.startswith("v:4"):
         print "ERROR: papers column not v:4"
         return
 
+    # these are the colmuns relevant for new profile format
+    origTags = row[1]
+    origNotes = row[2]
+    origGraphs = row[3]
+    if len(origGraphs) > 0:
+        origGraphsJson = json.loads(origGraphs)
+    else:
+        origGraphsJson = []
+
+    # parse old papers column
     lexer = MiniLexer(papers[3:])
     papers = []
     while not lexer.isEnd():
@@ -86,10 +100,20 @@ def doWork(dbCursor, usermail, dryRun):
         lexer.getChar(',')
         notes = lexer.getString()
         lexer.getChar(',')
+
+        # labels (old graphs?)
         lexer.getChar('l')
         lexer.getChar('[')
+        labels = []
+        while not lexer.isChar(']'):
+            label = lexer.getString()
+            labels.append(label)
+            if lexer.isChar(','):
+                lexer.getChar(',')
         lexer.getChar(']')
         lexer.getChar(',')
+
+        # tags
         lexer.getChar('t')
         lexer.getChar('[')
         tags = []
@@ -99,10 +123,11 @@ def doWork(dbCursor, usermail, dryRun):
             if lexer.isChar(','):
                 lexer.getChar(',')
         lexer.getChar(']')
+
         lexer.getChar(')')
 
-        papers.append((id, xpos, rmod, notes, tags))
-        print id, xpos, rmod, notes, tags
+        papers.append((id, xpos, rmod, notes, tags, labels))
+        print id, xpos, rmod, notes, tags, labels
 
     # make tags column
     tags = {}
@@ -117,8 +142,15 @@ def doWork(dbCursor, usermail, dryRun):
     for i in xrange(len(tags)):
         tags[i][1] = i
         tags[i][2].sort()
-    
-    tagsColumn = "[{}]".format(','.join("{{\"name\":\"{}\",\"ind\":{},\"blob\":false,\"ids\":[{}]}}".format(tagName, tagIndex, ','.join(str(id) for id in tagIds)) for tagName, tagIndex, tagIds in tags))
+
+    if len(origTags) > 0:
+        if len(tags) != 0:
+            print "ERROR: cannot combine old and new tags (not implemented)"
+            return
+        print "using original tags column"
+        tagsColumn = origTags
+    else:
+        tagsColumn = "[{}]".format(','.join("{{\"name\":\"{}\",\"ind\":{},\"blob\":false,\"ids\":[{}]}}".format(tagName, tagIndex, ','.join(str(id) for id in tagIds)) for tagName, tagIndex, tagIds in tags))
 
     # make notes column
     notes = []
@@ -127,10 +159,52 @@ def doWork(dbCursor, usermail, dryRun):
             notes.append((paper[0], paper[3]))
     notes.sort(lambda x, y: cmp(x[0], y[0]))
 
-    notesColumn = "[{}]".format(','.join("{{\"id\":{},\"notes\":\"{}\"}}".format(note[0], note[1].replace('/', '\\/')) for note in notes))
+    if len(origNotes) > 0:
+        if len(notes) != 0:
+            print "ERROR: cannot combine old and new notes (not implemented)"
+            return
+        print "using original notes column"
+        notesColumn = origNotes
+    else:
+        notesColumn = "[{}]".format(','.join("{{\"id\":{},\"notes\":\"{}\"}}".format(note[0], note[1].replace('/', '\\/')) for note in notes))
+
+    # make graphs column
+    graphs = {}
+    for paper in papers:
+        for l in paper[5]:
+            if l not in graphs:
+                graphs[l] = ["Old graph {}".format(l), 0, [(paper[0], paper[1], paper[2])]]
+            else:
+                graphs[l][2].append((paper[0], paper[1], paper[2]))
+    graphs = graphs.values()
+    graphs.sort(lambda x, y: cmp(x[0], y[0]))
+    for i in xrange(len(graphs)):
+        graphs[i][1] = len(origGraphsJson) + i
+        graphs[i][2].sort(lambda x, y: cmp(x[0], y[0]))
+
+    if len(graphs) == 0:
+        print "using original graphs column"
+        graphsColumn = origGraphs
+    else:
+        # graphs of the format: [{"name":"Graph1","ind":<index>,"drawn":[{"id":<id>,"x":<x>,"r":<r>},{..}]},{"name":...},...]
+        graphsColumnInner = ','.join("{{\"name\":\"{}\",\"ind\":{},\"drawn\":[{}]}}".format(graphName, graphIndex, ','.join("{{\"id\":{},\"x\":{},\"r\":{}}}".format(id, x, r) for id, x, r in graphPapers)) for graphName, graphIndex, graphPapers in graphs)
+        if origGraphs == '[]':
+            print "converted {} new graphs".format(len(graphs))
+            graphsColumn = '[' + graphsColumnInner + ']'
+        elif origGraphs.startswith('[{') and origGraphs.endswith('}]'):
+            print "merging {} old graphs with {} new graphs".format(len(origGraphsJson), len(graphs))
+            graphsColumn = origGraphs[:-1] + ',' + graphsColumnInner + ']'
+        else:
+            print "ERROR: origGraphs string is of wrong format"
+            return
+
+    # verify our string are valid JSON
+    json.loads(tagsColumn)
+    json.loads(notesColumn)
+    json.loads(graphsColumn)
 
     # update the userdata table
-    executeSqlQuery(dbCursor, dryRun, "UPDATE userdata SET tags=%s, notes=%s WHERE usermail=%s", (tagsColumn, notesColumn, usermail))
+    executeSqlQuery(dbCursor, dryRun, "UPDATE userdata SET tags=%s, notes=%s, graphs=%s WHERE usermail=%s", (tagsColumn, notesColumn, graphsColumn, usermail))
 
 if __name__ == "__main__":
 
