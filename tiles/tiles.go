86e5ff41ac48711ca9d2d4d7d50515596de23d05
Damien George <damien.p.george@gmail.com>
2013-07-18 13:00:37 +0000

    small changes to colour style in tiles

--- tiles/tiles.go
+++ tiles/tiles.go
@@ -356,7 +356,7 @@ func (paper *Paper) setColour() {
         // older papers are saturated and dark, newer papers are coloured and bright
         //saturation := 0.4 * (1 - age)
         saturation := 0.0
-        dim_factor := 0.5 * (1 + age)
+        dim_factor := 0.25 + 0.75 * age
         r = dim_factor * (saturation + r * (1 - saturation))
         g = dim_factor * (saturation + g * (1 - saturation))
         b = dim_factor * (saturation + b * (1 - saturation))
@@ -694,9 +694,9 @@ func DrawTile(graph *Graph, worldWidth, worldHeight, xi, yi, surfWidth, surfHeig
     // background (just the new papers)
     graph.qt.ApplyIfWithin(int(x), int(y), int(rx)+graph.qt.MaxR, int(ry)+graph.qt.MaxR, func(paper *Paper) {
         if paper.id > 2133969854 {
-            // testing: new papers today have a red background
+            // testing: new papers today have a white background
             surf.Arc(float64(paper.x), float64(paper.y), 100, 0, 2 * math.Pi)
-            surf.SetSourceRGB(0.5, 0, 0)
+            surf.SetSourceRGB(0.8, 0.8, 0.8)
             surf.Fill()
         }
     })
