1e5c9507811c50844e68c2799fed8f3fb5e23262
Rob Knegjens <rknegjens@gmail.com>
2013-07-26 14:22:11 +0200

    improved kw selection, more tiling for label zones

--- labels/labels.go
+++ labels/labels.go
@@ -146,7 +146,7 @@ func cleanJsonString(input string) string {
     // TODO work out exactly which chars are causing
     // parsing error and blacklist or escape them
     // inplace of this whitelist
-    validChars := []byte{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0'}
+    validChars := []byte{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0',' '}
 
     for _, b := range(inbytes) {
         for _, c := range validChars {
@@ -548,11 +548,12 @@ func GenerateAllLabelZones(graph *Graph, outPrefix string) {
         {1,1},
         {1,2},
         {1,4},
-        {1,8},
-        {2,16},
-        {4,32},
-        {8,64},
-        {16,128},
+        {2,8},
+        {4,16},
+        {8,32},
+        {16,64},
+        {32,128},
+        {64,256},
     }
 
     first := true
